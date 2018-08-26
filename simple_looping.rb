8eb4758d03eea097b4376e289d9ed9bd26898e7a
Jackie Ha <jha2@wellesley.edu>
2018-08-26 13:24:35 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,7 @@
 def loop_iterator(number_of_times)
   counter = 0
   loop do 
-   counter = counter + 1 
+   counter += 1 
    puts "#{counter} Welcome to Flatiron School's Web Development Course!"
    if counter >= 7
      break
